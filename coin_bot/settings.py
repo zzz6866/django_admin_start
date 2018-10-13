06ee1f25d99e345d73a52a84456f13ac6e6d1ae8
Mr. Park <zzz6866@gmail.com>
2018-10-13 19:15:52 +0900

    ALLOWED_HOSTS 추가

--- coin_bot/settings.py
+++ coin_bot/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'hbuipsj#g5n!c)r-c8@7i(1kegsf9##r*#&8$m6f(6$&b_(!)7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.2', 'localhost', '127.0.0.1']
 
 # Application definition
 
