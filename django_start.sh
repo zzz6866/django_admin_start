68ec1c0492b124542eb26dc09026ae66fdfd2e7a
park <zzz6866@gmail.com>
2019-01-14 19:03:57 +0900

    sh 커밋

--- django_start.sh
+++ django_start.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+celery -A coin_bot worker -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A coin_bot beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 python manage.py runserver 192.168.0.2:8000 --settings=coin_bot.settings.production &
