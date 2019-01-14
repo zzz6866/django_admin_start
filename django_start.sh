8286adf74a3081b82310a11200bf2c73af66529b
Mr. Park <zzz6866@gmail.com>
2019-01-14 20:00:01 +0900

    sh 커밋(프로젝트 실행용)

--- django_start.sh
+++ django_start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# set DJANGO_SETTINGS_MODULE=coin_bot.settings.local #  Windows shell
+export DJANGO_SETTINGS_MODULE=coin_bot.settings.local # Unix Bash shell
+
 celery -A coin_bot worker -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 celery -A coin_bot beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 python manage.py runserver 192.168.0.2:8000 --settings=coin_bot.settings.production &
