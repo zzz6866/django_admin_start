470fea511c6f4223da07ccc8a95c4f6da862c302
park <zzz6866@gmail.com>
2019-01-21 13:47:40 +0900

    setting config 변경

--- django_start.sh
+++ django_start.sh
@@ -4,4 +4,4 @@ export DJANGO_SETTINGS_MODULE=coin_bot.settings.production # Unix Bash shell
 
 celery -A coin_bot worker -B -l INFO &
 #celery -A coin_bot beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler & # worker -B 옵션으로 실행하지 않아도 DB 스케쥴 사용
-python manage.py runserver localhost:8000 --settings=coin_bot.settings.production &
+python manage.py runserver 192.168.0.2:8000 --settings=coin_bot.settings.production &
