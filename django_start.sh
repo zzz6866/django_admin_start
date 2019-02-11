87a087c526d9091167b10c54bb26be7992242e1f
park <zzz6866@gmail.com>
2019-02-11 16:32:27 +0900

    shell 수정

--- django_start.sh
+++ django_start.sh
@@ -10,11 +10,11 @@ if [[ *"local"* == "$DJANGO_SETTINGS_MODULE" ]]; then
     # brew  # redis start
     nohup celery -A alldev worker -B -l INFO &
     sleep 10
-    curl -X GET https://local.alldev.co.kr:8443/torrent/webhook/
+    curl -X GET https://local.alldev.co.kr:8443/torrent/setWebhook/
 else
     nohup python manage.py runserver 0.0.0.0:8000 & # django run
     /etc/init.d/redis-server restart # redis start
     service celeryd restart # 데몬 실행
     sleep 10
-    curl -X GET https://www.alldev.co.kr:8443/torrent/webhook/
+    curl -X GET https://www.alldev.co.kr:8443/torrent/setWebhook/
 fi
