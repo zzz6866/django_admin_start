207671b72e8c4f1fad17ccfdd85f72aba197ce05
park <zzz6866@gmail.com>
2019-02-13 00:21:07 +0900

    일일 신규 토렌트 크롤링 2페이지 이상 있는 경우 수젇

--- torrent_bot/selenium_chrome.py
+++ torrent_bot/selenium_chrome.py
@@ -64,39 +64,46 @@ class SeleniumChrome:
 
         # PhantomJS의 경우 아까 받은 PhantomJS의 위치를 지정해준다.
         # driver = webdriver.PhantomJS(executable_path=BASE_DIR + r'/selenium/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-
-        chromedriver.get(BASE_URL + "/torrent_movie/torrent1.htm")
-        html_list = chromedriver.page_source
-        # print(html)
-        soup = BeautifulSoup(html_list, 'html.parser')
-        tr_board_list = soup.select(TR_BOARD_LIST)  #
-        # print(tr_board_list)
+        page = 1
         res_list = []
-        for tr in tr_board_list:
-            strong_today = tr.find('td', attrs={'class': 'datetime'}).find('strong', recursive=False)
-            if strong_today:
-                a_href = tr.find('td', attrs={'class': 'subject'}).find('a', recursive=False)
-                detail_url = BASE_URL + a_href['href'].replace('..', '')  # 상세 화면 url
-                dic = {"torrent_title": a_href.string, "torrent_detail_url": detail_url}
-                res_list.append(dic)
+        while True:
+            get_url = BASE_URL + "/torrent_movie/torrent" + str(page) + ".htm"
+            chromedriver.get(get_url)
+            html_list = chromedriver.page_source
+            # print(html)
+            soup = BeautifulSoup(html_list, 'html.parser')
+            tr_board_list = soup.select(TR_BOARD_LIST)  #
+            # print(tr_board_list)
+            for tr in tr_board_list:
+                strong_today = tr.find('td', attrs={'class': 'datetime'}).find('strong', recursive=False)
+                # strong_today = tr.find('td', attrs={'class': 'datetime'})
+                if strong_today:
+                    a_href = tr.find('td', attrs={'class': 'subject'}).find('a', recursive=False)
+                    detail_url = BASE_URL + a_href['href'].replace('..', '')  # 상세 화면 url
+                    dic = {"torrent_title": a_href.string, "torrent_detail_url": detail_url}
+                    res_list.append(dic)
 
-                # 상세 화면에서 torrent or magnet url을 뽑아 다운로드 목록에 추가하는 로직 (미구현)
-                # print(torrent_idx)
-                # chromedriver.get(detail_url)
-                # html_detail = chromedriver.page_source
-                # soup = BeautifulSoup(html_detail, 'html.parser')
-                # table_file = soup.find('table', attrs={'id': 'file_table'}).find_all('tr')
-                # print(table_file)
-                # for i, detail_tr in enumerate(table_file):
-                #     download_url = detail_tr.find('td').find('a', recursive=False)
-                #     if download_url is not None and "magnet" not in str(download_url):
-                #         print(i, "magnet" in str(download_url), download_url['href'])
-                #         if "javascript" in str(download_url['href']).lower():
-                #             print("javascript", )
-                #         else:
-                #             print("Not javascript")
-                #
-                #         chromedriver.find_element_by_css_selector('#file_table > tbody > tr:nth-child(' + str(i + 1) + ') > td > a').click()
+                    # 상세 화면에서 torrent or magnet url을 뽑아 다운로드 목록에 추가하는 로직 (미구현)
+                    # print(torrent_idx)
+                    # chromedriver.get(detail_url)
+                    # html_detail = chromedriver.page_source
+                    # soup = BeautifulSoup(html_detail, 'html.parser')
+                    # table_file = soup.find('table', attrs={'id': 'file_table'}).find_all('tr')
+                    # print(table_file)
+                    # for i, detail_tr in enumerate(table_file):
+                    #     download_url = detail_tr.find('td').find('a', recursive=False)
+                    #     if download_url is not None and "magnet" not in str(download_url):
+                    #         print(i, "magnet" in str(download_url), download_url['href'])
+                    #         if "javascript" in str(download_url['href']).lower():
+                    #             print("javascript", )
+                    #         else:
+                    #             print("Not javascript")
+                    #
+                    #         chromedriver.find_element_by_css_selector('#file_table > tbody > tr:nth-child(' + str(i + 1) + ') > td > a').click()
+            if len(res_list) == 30 * page:
+                page += 1
+            else:
+                break
         # Chrome Driver Close
         chromedriver.close()
         return res_list
