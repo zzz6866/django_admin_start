2a30643ab40658b7560132ec3c0451c954aabc99
park <zzz6866@gmail.com>
2019-11-15 22:31:22 +0900

    dll 테스트 소스 커밋

--- flipper_helper/tests.py
+++ flipper_helper/tests.py
@@ -5,8 +5,9 @@ from django.test import TestCase
 if __name__ == "__main__":
     import zugbruecke as ctypes
 
-    simple_demo_routine = ctypes.windll.LoadLibrary('demo_dll.dll').simple_demo_routine
-    simple_demo_routine.argtypes = (ctypes.c_float, ctypes.c_float)
-    simple_demo_routine.restype = ctypes.c_float
-    return_value = simple_demo_routine(20.0, 1.07)
-    print('Got "%f".' % return_value)
+    simple_demo_routine = ctypes.windll.LoadLibrary('bin/wmca.dll')
+    simple_demo_routine.wmcaIsConnected()
+    # simple_demo_routine.argtypes = (ctypes.c_float, ctypes.c_float)
+    # simple_demo_routine.restype = ctypes.c_float
+    # return_value = simple_demo_routine(20.0, 1.07)
+    # print('Got "%f".' % return_value)
