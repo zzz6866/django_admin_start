cc43567aa5ad4a8121415750ecf0429c769709d6
park <zzz6866@gmail.com>
2019-11-15 22:15:48 +0900

    dll 테스트 소스 커밋

---
+++ flipper_helper/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+if __name__ == "__main__":
+    import zugbruecke as ctypes
+
+    simple_demo_routine = ctypes.windll.LoadLibrary('demo_dll.dll').simple_demo_routine
+    simple_demo_routine.argtypes = (ctypes.c_float, ctypes.c_float)
+    simple_demo_routine.restype = ctypes.c_float
+    return_value = simple_demo_routine(20.0, 1.07)
+    print('Got "%f".' % return_value)
