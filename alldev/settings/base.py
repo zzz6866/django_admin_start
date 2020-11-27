0444dc07f23db074f70b2edc69b34ced4bff5e6f
Mr. Park <zzz6866@gmail.com>
2020-11-27 13:09:46 +0900

    로그 출력 포맷 변경 (파일명:라인 추가)

--- alldev/settings/base.py
+++ alldev/settings/base.py
@@ -164,7 +164,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d: %(message)s',
+            'format': '%(levelname)s %(asctime)s (%(filename)s:%(lineno)d) : %(message)s',
         },
         'simple': {
             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
