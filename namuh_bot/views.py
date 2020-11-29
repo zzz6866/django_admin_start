08c50f4c5ddf639db254369d429f0de97b5e6b1b
park <zzz6866@gmail.com>
2020-11-29 17:42:39 +0900

    celery, model 수정

--- namuh_bot/views.py
+++ namuh_bot/views.py
@@ -10,6 +10,7 @@ def index(request):
 
 
 class CDAutocompleteView(autocomplete.Select2QuerySetView):
+    # 종목 코드 ajax 처리를 위한 view 추가 (foreignKey 로 연결 되어 있으나 페이지 로드에 코드 가져올 경우 느리기 때문에 추가)
     def get_queryset(self):
         qs = CD.objects.all()
         if self.q:
